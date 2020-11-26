3e08306256b73a97335912a41ef012665a816f7b
ivgomez <iv.gomez@hotmail.com>
2020-11-26 15:43:23 -0500

    auth 0 button

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -4,23 +4,27 @@ import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/ToolBar";
 import Button from "@material-ui/core/Button";
+import LoginButton from "./Login/Login";
 
 const NavBar = (props) => {
     return (
         <AppBar>
             <ToolBar className="nav-container">
-                <Button color="inherit" component={Link} to="/">
-                    Home
-                </Button>
-                <Button color="inherit" component={Link} to="/tutorias">
-                    Tutorias
-                </Button>
-                <Button color="inherit" component={Link} to="/personalizadas">
-                    TutoriasPersonalizadas
+                <div className="menu-container">
+                    <div class="logo">
+                        <img src="https://ustadistancia.edu.co/images/logo_blanco_440b.png" alt="logo" height={30} />
+                    </div>
+                    <Button color="inherit" component={Link} to="/">
+                        Home
+                    </Button>
+                    <Button color="inherit" component={Link} to="/tutorias">
+                        Tutorias
+                    </Button>
+                    <Button color="inherit" component={Link} to="/personalizadas">
+                        TutoriasPersonalizadas
                  </Button>
-                <Button color="inherit" component={Link} to="/">
-                    Login
-                </Button>
+                </div>
+                <LoginButton />
             </ToolBar>
         </AppBar>
     );
